5f3c59230ecd8103a0deee10bb4d2dc2aa71e940
汤星明 <xmtang@ittx.com.cn>
2024-09-10 12:25:33 +0800

    mac 无/dev/net/tun设备，容器内手动创建 3

--- start.sh
+++ start.sh
@@ -5,11 +5,13 @@ if [ -e /dev/net/tun ]; then
   :
 else
   # mac 无/dev/net/tun设备，容器内手动创建
-  sudo mkdir /dev/net
-  sudo mknod /dev/net/tun c 10 200
-  sudo ip tuntap add mode tap tap
+  # sudo mkdir /dev/net
+  # sudo mknod /dev/net/tun c 10 200
+  # sudo ip tuntap add mode tap tap
   #echo "/dev/net/tun doesn't exist. Please create it first." >&2
   #exit 1
+  mkdir /dev/net
+  mknod -m 0666 /dev/net/tun c 10 200
 fi
 
 # 定义一个处理函数，用于在接收到 INT 信号时退出脚本
