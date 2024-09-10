db4202758f34d8fc367266da0d6689b6d6397405
汤星明 <xmtang@ittx.com.cn>
2024-09-10 15:20:53 +0800

    使用gost做代理服务5

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # 下载gost
+pla="linux_arm64"
 fileName="gost_3.0.0-nightly.20240904_$pla.tar.gz"
 gost_base_url="https://github.com/go-gost/gost/releases/download/v3.0.0-nightly.20240904/$fileName"
-pla="linux_arm64"
 mkdir gost_arm64
 echo "下载 $gost_base_url"
 wget $gost_base_url
 tar zxvf $fileName -C ./gost_arm64
 chmod +x ./gost_arm64/gost
 pla="linux_amd64"
+fileName="gost_3.0.0-nightly.20240904_$pla.tar.gz"
+gost_base_url="https://github.com/go-gost/gost/releases/download/v3.0.0-nightly.20240904/$fileName"
 mkdir gost_amd64
 echo "下载 $gost_base_url"
 wget $gost_base_url
